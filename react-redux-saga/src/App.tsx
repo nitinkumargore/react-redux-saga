4375cada74656c8b1f3b5d86d974f24b3c814519
Nitinkumar Gore <ni3.gore@gmail.com>
2024-02-16 13:08:25 -0700

    added some styles and one todo in the list

--- react-redux-saga/src/App.tsx
+++ react-redux-saga/src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const todos:Todo[] = [{
+    id:1,
+    name:'learn Rexus',
+    isActive:true,
+    isDone:false
+  }]
+
+  const [newTodoText, setNewTodoText] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <div className='flex flex-col items-center gap-2'>
+        <h1 className='text-2xl font-bold'>Dynamic ToDo List</h1>
+        <div className='border-t border-gray-200 mb-2 w-[80%]'/>
+        <div>
+           {
+            todos?.map((td:Todo)=>
+            <div className='flex flex-end flex-gap-10 flex-[10_2_2%] items-center min-h-[40px]'>
+              <div className='min-w-[25px] mt-[7px]'><input type='checkbox' className='h-[18px] w-[18px]'/></div>
+              <div className='min-w-[400px] w-[400px] break-words'>{td.name}</div>
+              <div className='min-w-[100px]'><button className='p-1 px-3 bg-blue-600 text-white rounded'>Edit</button></div>
+              <button className='p-1 px-3 bg-blue-600 text-white rounded'>Delete</button>
+            </div>)
+          } 
+         </div>
+
+         <div className='border-t border-gray-200 mb-2 w-[80%]'/>
+
+         <div>
+            <div className='text-center text-l font-bold'>Add new item to todo list.</div>
+            <input className='mx-2 border p-1 rounded min-w-[300px]' 
+              name='newTodoInput' 
+              placeholder='Enter your todo text here' 
+              value={newTodoText}
+              onChange={(e)=>setNewTodoText(e.target.value)}
+              />
+            <button className='mx-2 bg-blue-600 rounded p-1 px-3 text-white'
+              disabled={!Boolean(newTodoText)}
+            >Add Todo</button>
+         </div>
+      </div>
     </div>
   );
 }
---
+++ react-redux-saga/src/global.d.ts
@@ -0,0 +1,6 @@
+interface Todo{
+    id:number,
+    name:string,
+    isDone:boolean,
+    isActive:boolean,
+}
